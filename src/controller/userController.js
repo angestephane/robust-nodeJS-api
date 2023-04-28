c6754bfa25e48f5b3d61c9747aa3a9ddfdc54130
ange stephane <boguhestephane97@gmail.com>
2023-04-28 18:48:28 +0100

    update readme file

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -14,3 +14,17 @@ module.exports.createUser = async (req, res) => {
   }
   return res.status(response.status).send(response);
 };
+
+module.exports.loginUser = async (req, res) => {
+  const response = { ...constants.defaultServerMessage };
+  try {
+    const data = await userService.loginUser(req.body);
+    response.status = 201;
+    response.message = constants.userMessage.LOGIN_SUCCESS;
+    response.body = data;
+  } catch (err) {
+    response.status = 400;
+    response.message = `Désolé, l'utilisateur n'est pas été crée, ${err.message}`;
+  }
+  return res.status(response.status).send(response);
+};
